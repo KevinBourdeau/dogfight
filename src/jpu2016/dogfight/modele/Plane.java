7b68ca34359feb2c87876d9ffcb20960f2cf695c
BOURTHOUMIEUXThomas <33619462+BOURTHOUMIEUXThomas@users.noreply.github.com>
2018-04-25 10:49:52 +0200

    Plane

--- src/jpu2016/dogfight/modele/Plane.java
+++ src/jpu2016/dogfight/modele/Plane.java
@@ -1,5 +1,15 @@
 package jpu2016.dogfight.modele;
 
 public class Plane {
-
+	static int SPEED = 4;
+	static int WIDTH = 100;
+	static int HEIGHT = 30;
+	int player;
+	
+	public Plane (int player, Direction direction, Position position, String image) {	
+	}
+	public boolean isPlayer (int player) {
+	}
+	public boolean hit() {
+	}	
 }
