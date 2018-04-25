ead3e64b6e652108bffa28d5bc5547279fa912b6
BOURTHOUMIEUXThomas <33619462+BOURTHOUMIEUXThomas@users.noreply.github.com>
2018-04-25 10:42:11 +0200

    Position.Java

--- src/jpu2016/dogfight/modele/Position.java
+++ src/jpu2016/dogfight/modele/Position.java
@@ -6,5 +6,36 @@ public class Position {
 	protected double maxX;
 	protected double maxY;
 	
+	public Position (double x, double y, double maxX, double maxY) {
+	}
+
+	public Position (Position Position) {
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setMaxX(double maxX) {
+		this.maxX = maxX;
+	}
+
+	public void setMaxY(double maxY) {
+		this.maxY = maxY;
+	}
+
+	
 	
 }
